f0f566f586c5de0a875d8063bebcd09c9c2eb2b5
Katherin Acevedo <katherin.acevedo.8747@miremington.edu.co>
2025-06-10 19:41:20 -0500

    configuracion de la carpeta app.py

--- app.py
+++ app.py
@@ -7,14 +7,20 @@ from dotenv import load_dotenv
 
 
 # TODO: Cargar variables de entorno desde archivo .env si existe
+load_dotenv()
 
 # TODO: crear la aplicación
+app = create_app()
 
 if __name__ == "__main__":
     # TODO: Obtener puerto del ambiente o usar 5000 por defecto
+    port = int(os.environ.get('PORT', 5000))
     
     # TODO: Determinar si se debe usar modo debug
+    debug_mode = (os.environ.get('FLASK_DEBUG') == '1' or 
+                  os.environ.get('FLASK_ENV') == 'development')
     
     # TODO: Ejecutar aplicación
+    app.run(host='0.0.0.0', port=port, debug=debug_mode)
 
     pass
